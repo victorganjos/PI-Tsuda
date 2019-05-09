99e127f95681f92eff9bb0a3d3b067362be967e8
victor.ganjos <victor.ganjos@CAS554824W10D09.senacsp.edu.br>
2019-05-09 18:46:04 -0300

    "Página de inserir usuário att."

--- developing-version/projeto-x/src/main/java/com/projetox/web/AtualizarUsuario.java
+++ developing-version/projeto-x/src/main/java/com/projetox/web/AtualizarUsuario.java
@@ -11,15 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import com.projetox.web.controller.UsuarioController;
 import com.projetox.web.model.Usuario;
 import java.util.List;
-
-
-
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "AtualizarUsuario", urlPatterns = {"/atualizar-usuario"})
-public class AtualizarUsuario extends HttpServlet{
+public class AtualizarUsuario extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
+            throws ServletException, IOException {
         //recupera as informações do formulario
         String escondido = request.getParameter("escondido");
         String id = request.getParameter("id");
@@ -28,9 +27,7 @@ public class AtualizarUsuario extends HttpServlet{
         String senha = request.getParameter("senha");
         String cargo = request.getParameter("cargo");
         String filial = request.getParameter("filial");
-        
-        
-        
+
         //Armazena valores como atributos 
         request.setAttribute("metodoHttp", "POST");
         request.setAttribute("escondido", escondido);
@@ -40,24 +37,27 @@ public class AtualizarUsuario extends HttpServlet{
         request.setAttribute("senha", senha);
         request.setAttribute("cargo", cargo);
         request.setAttribute("filial", filial);
-        
-        
-        UsuarioController.Atualizar(Integer.parseInt(id), nome,username, senha, cargo, filial);
- 
+
+        UsuarioController.Atualizar(Integer.parseInt(id), nome, username, senha, cargo, filial);
+
+        List<Usuario> lista = UsuarioController.consultar();
+        request.setAttribute("consulta", lista);
         RequestDispatcher dispatcher
-                = request.getRequestDispatcher("index.jsp");
+                = request.getRequestDispatcher("/WEB-INF/jsp/ConsultaUsuario.jsp");
         dispatcher.forward(request, response);
     }
 
-        @Override
-     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
-         
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         int id = Integer.parseInt(request.getParameter("id"));
-         
-        List<Usuario> lista = UsuarioController.pesquisar(id);
-        request.setAttribute("id", id);
-        request.setAttribute("consulta", lista);
+
+        HttpSession sessao = request.getSession();
+
+        List<Usuario> acesso = UsuarioController.pesquisar(id);
+        sessao.setAttribute("id", id);
+        sessao.setAttribute("acesso", acesso);
         RequestDispatcher dispatcher
                 = request.getRequestDispatcher("/atualizarUsuario.jsp");
         dispatcher.forward(request, response);
